f5a65b83d3d7d14930c7a075a1c9fa11d8d1a401
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-03 21:20:37 +0100

    throwing errors manually

--- script.js
+++ script.js
@@ -168,14 +168,21 @@ const getCountryData = function (countryName) {
 // getCountryData('nigeria');
 
 const getCountryAndNeighbour = function (countryName) {
-  getJSON(`https://restcountries.com/v3.1/name/${countryName}`)
+  getJSON(
+    `https://restcountries.com/v3.1/name/${countryName}`,
+    'Country not found'
+  )
     .then(data => {
       renderCountry(data[0]);
       const [neighbour] = data[0].borders;
+      // const neighbour = 'ssssssss';
 
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      // return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        'Country not found'
+      );
     })
-    .then(response => response.json())
     .then(data => renderCountry(data[0], 'neighbour'))
     .catch(err => {
       console.error(err.message + '!!');
