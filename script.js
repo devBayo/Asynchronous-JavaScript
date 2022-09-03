a0a7a255f1347f3f6b3c129721e13a41a95d86a5
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-03 08:40:24 +0100

    call back hell

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const countriesContainer = document.querySelector('.countries');
 ////////////////////////////////////////
 /* Old way of fetching data */
 /*
+
 const getCountryData = function (countryName) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
@@ -48,3 +49,56 @@ getCountryData('france');
 
 ////////////////////////////
 // Call back hell
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${Number(
+        +data.population / 1000000
+      ).toFixed(1)}M people</p>
+      <p class="country__row"><span>🗣️</span>${
+        data.languages[Object.keys(data.languages)[0]]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies[Object.keys(data.currencies)[0]].name
+      }</p>
+    </div>
+  </article>
+`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (countryName) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    renderCountry(data);
+
+    const [neighbour] = data.borders;
+    console.log(neighbour);
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data] = JSON.parse(this.responseText);
+      console.log(data);
+      renderCountry(data, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('nigeria');
+getCountryAndNeighbour('usa');
