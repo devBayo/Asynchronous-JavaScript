b61652f0022d252cd6cc77fc8d8603eba375886b
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-07 16:57:04 +0100

    promisifying geolocation api

--- script.js
+++ script.js
@@ -283,9 +283,13 @@ console.log('Getting Position');
 
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
+
+getPosition().then(res => console.log(res));
