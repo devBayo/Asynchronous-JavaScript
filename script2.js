a7dfd40eb948dddc52a5c2fee1528752940197dc
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-16 04:55:00 +0100

    running promises in parallel

--- script2.js
+++ script2.js
@@ -15,7 +15,7 @@ const get3Countries = async function (c1, c2, c3) {
 
     // console.log([data1.capital[0], data2.capital[0], data3.capital[0]]);
     //// Promise.all short circuit one a promise is rejected
-    
+
     const data = await Promise.all([
       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
@@ -83,3 +83,19 @@ const timeout = function (seconds) {
 })();
 
 //// Promise.allSettled
+/*
+Promise.allSettled doesn't short circuit
+*/
+(async function () {
+  try {
+    const response = await Promise.allSettled([
+      Promise.resolve('Resolved 1'),
+      Promise.reject('Error'),
+      Promise.resolve('Resolved 2'),
+    ]);
+
+    console.log(response);
+  } catch (error) {
+    console.warn(error);
+  }
+})();
